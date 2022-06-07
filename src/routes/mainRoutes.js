0d52510a983bdb2ed992f0880c7bee4c7b1b927a
Jorge Polanco <55784702+Jorge-Polanco-Roque@users.noreply.github.com>
2022-06-06 21:02:38 -0500

    Delete src directory

--- src/routes/mainRoutes.js
+++
@@ -1,7 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const controladorPrincipal = require("../controllers/mainController");
-
-router.get("/", controladorPrincipal.index);
-
-module.exports = router;
